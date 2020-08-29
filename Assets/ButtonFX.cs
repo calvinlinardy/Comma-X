a66663e73a4384ae2c21b4f2dd842f3947d08132
calvinlinardy <calvin.linardy@yahoo.com>
2020-08-29 19:06:54 +0800

    UI updates.
    - button animations.
    - game over UI updates.

---
+++ Assets/ButtonFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+    public AudioSource myFX;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+    [SerializeField] [Range(0, 1)] float SFXVolume = 0.7f;
+
+    public void HoverSound()
+    {
+        myFX.PlayOneShot(hoverFX, SFXVolume);
+    }
+
+    public void ClickSound()
+    {
+        myFX.PlayOneShot(clickFX, SFXVolume);
+    }
+}
